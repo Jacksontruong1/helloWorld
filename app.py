caad0d17bc25ebb5c45b9e4747a57f6ad0025c93
Jackson2 <jackson2@umd.edu>
2023-02-15 20:33:49 -0500

    adding new route for about.html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Jackson Truong! I am adding my first code change.'
+    return 'Hello World from Jackson Truong! I am adding my first code changes.'
 
 @app.route('/hello')
 def hello():
